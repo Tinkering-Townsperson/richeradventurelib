c22b3723bdf47e793c0d6eb268bc25b917a25fb3
Afternoon Pm <h2o.Drop2010@gmail.com>
2026-03-22 21:48:47 -0700

    feat: Modify demo to have simple examples

--- demo_game.py
+++ demo_game.py
@@ -1,83 +1,85 @@
-from adventurelib import *
+import richeradventurelib as adv
 
-Room.items = Bag()
+adv.Room.items = adv.Bag()
 
-current_room = starting_room = Room("""
-You are in a dark room.
+current_room = starting_room = adv.Room("""
+You are in a [bold white on black]dark[/] room.
 """)
 
-valley = starting_room.north = Room("""
+valley = starting_room.north = adv.Room("""
 You are in a beautiful valley.
 """)
 
-magic_forest = valley.north = Room("""
+magic_forest = valley.north = adv.Room("""
 You are in a enchanted forest where magic grows wildly.
 """)
 
-mallet = Item('rusty mallet', 'mallet')
-valley.items = Bag({mallet,})
+mallet = adv.Item('rusty mallet', 'mallet')
+valley.items = adv.Bag({mallet, })
 
-inventory = Bag()
+inventory = adv.Bag()
 
 
-@when('north', direction='north')
-@when('south', direction='south')
-@when('east', direction='east')
-@when('west', direction='west')
+@adv.when('north', direction='north')
+@adv.when('south', direction='south')
+@adv.when('east', direction='east')
+@adv.when('west', direction='west')
 def go(direction):
-    global current_room
-    room = current_room.exit(direction)
-    if room:
-        current_room = room
-        say('You go %s.' % direction)
-        look()
-        if room == magic_forest:
-            set_context('magic_aura')
-        else:
-            set_context('default')
+	global current_room
+	room = current_room.exit(direction)
+	if room:
+		current_room = room
+		adv.say('You go [italic]%s[/].' % direction)
+		look()
+		if room == magic_forest:
+			adv.set_context('magic_aura')
+		else:
+			adv.set_context('default')
 
 
-@when('take ITEM')
+@adv.when('take ITEM')
 def take(item):
-    obj = current_room.items.take(item)
-    if obj:
-        say('You pick up the %s.' % obj)
-        inventory.add(obj)
-    else:
-        say('There is no %s here.' % item)
+	obj = current_room.items.take(item)
+	if obj:
+		adv.say('You pick up the [bold]%s[/].' % obj)
+		inventory.add(obj)
+	else:
+		adv.say('There is no [bold]%s[/] here.' % item)
 
 
-@when('drop THING')
+@adv.when('drop THING')
 def drop(thing):
-    obj = inventory.take(thing)
-    if not obj:
-        say('You do not have a %s.' % thing)
-    else:
-        say('You drop the %s.' % obj)
-        current_room.items.add(obj)
+	obj = inventory.take(thing)
+	if not obj:
+		adv.say('You do not have a [bold]%s[/].' % thing)
+	else:
+		adv.say('You drop the [bold]%s[/].' % obj)
+		current_room.items.add(obj)
 
 
-@when('look')
+@adv.when('look')
 def look():
-    say(current_room)
-    if current_room.items:
-        for i in current_room.items:
-            say('A %s is here.' % i)
+	adv.say(current_room)
+	if current_room.items:
+		for i in current_room.items:
+			adv.say('A [bold]%s[/] is here.' % i)
 
 
-@when('inventory')
+@adv.when('inventory')
 def show_inventory():
-    say('You have:')
-    for thing in inventory:
-        say(thing)
+	adv.say('You have:')
+	for thing in inventory:
+		adv.say('A [bold]%s[/] is here.' % thing)
 
-@when('cast', context='magic_aura', magic=None)
-@when('cast MAGIC', context='magic_aura')
+
+@adv.when('cast', context='magic_aura', magic=None)
+@adv.when('cast MAGIC', context='magic_aura')
 def cast(magic):
-    if magic == None:
-        say("Which magic you would like to spell?")
-    elif magic == 'fireball':
-        say("A flaming Fireball shoots form your hands!")
+	if magic is None:
+		adv.say("Which magic you would like to spell?")
+	elif magic == 'fireball':
+		adv.say("A :fire:[bold orange3]flaming Fireball[/]:fire: shoots from your hands!")
+
 
 look()
-start()
+adv.start()
